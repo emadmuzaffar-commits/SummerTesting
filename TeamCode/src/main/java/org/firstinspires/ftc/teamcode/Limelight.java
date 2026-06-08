220ff44d1315884773160f60b75d29cd25759b53
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2026-06-07 20:23:48 -0400

    code for pretty much all of it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight.java
@@ -4,19 +4,35 @@ package org.firstinspires.ftc.teamcode;
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Limelight {
     @Configurable
     public static class llConfig {
         public static int pipeline = 0;
+        public static double kR = 0.2;
     }
     private final Limelight3A ll;
     private final TelemetryManager telemetryM;
     public Limelight(HardwareMap hardwareMap) {
         ll = hardwareMap.get(Limelight3A.class, "ll");
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+    }
+
+    /// Loop method to detect an AprilTag and return Tx. Prints status to panels telemetry
+    private double getTx() {
+        double tx = ll.getLatestResult().getTx();
+        if (tx != 0) {
+            telemetryM.addData("status", "detected");
+        }
+        return tx;
+    }
+
+    private double getRotation(double tx) {
+        return tx * llConfig.kR;
     }
 
     /// Main init method.
@@ -26,10 +42,14 @@ public class Limelight {
         ll.start();
     }
 
-    /// Method to detect an AprilTag. Prints to panels telemetry
-    public void aprilTagID() {
-
+    /// Main loop method
+    public void loop(Follower follower, Gamepad gamepad1) {
+        //get the tx from ll
+        double tx = getTx();
+        //apply rotation multiplier
+        double r = getRotation(tx);
+        //setTeleopDrive
+        follower.setTeleOpDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, r);
     }
-
 
 }
