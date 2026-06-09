1b2c02a9deb0681d0be2850372722305e7c98fdc
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2026-06-09 11:14:49 -0400

    restrcuture

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MT2.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class MT2 extends OpMode {
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TagTrackerTele.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class TagTrackerTele extends OpMode {
+    private TagTracker tagTracker;
+
+    @Override
+    public void init() {
+        tagTracker = new TagTracker(hardwareMap);
+        tagTracker.follower.startTeleopDrive();
+        tagTracker.init();
+    }
+
+    @Override
+    public void loop() {
+        tagTracker.follower.update();
+        tagTracker.trackTag(gamepad1);
+    }
+
+    static class TagTracker extends TestingBase {
+        @Configurable
+        public static class tagTrackerConfig {
+            public static int pipeline = 0;
+            public static double kR = 0.2;
+        }
+
+        public TagTracker(HardwareMap hardwareMap) {
+            super(hardwareMap);
+        }
+
+        /// Loop method to detect an AprilTag and return Tx. Prints status to panels telemetry
+        private double getTx() {
+            double tx = ll.getLatestResult().getTx();
+            if (tx != 0) {
+                telemetryM.addData("status", "detected");
+            }
+            return tx;
+        }
+
+        private double getRotation(double tx) {
+            return tx * tagTrackerConfig.kR;
+        }
+
+        /// Main init method.
+        /// Sets the pipeline to tagTrackerConfig.pipeline and starts ll.
+        public void init() {
+            ll.pipelineSwitch(tagTrackerConfig.pipeline);
+            ll.start();
+        }
+
+        /// Main trackTag method. Sets teleop drive to be x,y controlled by gamepad with rotation to track tag.
+        /// Start teleop drive must be called before this
+        public void trackTag(Gamepad gamepad1) {
+            //get the tx from ll
+            double tx = getTx();
+            //apply rotation multiplier
+            double r = getRotation(tx);
+            //setTeleopDrive
+            follower.setTeleOpDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, r);
+        }
+
+    }
+}
